3ae4731dcc2177d6268831c49bdbce17297cf7d1
chanyan17 <yan_chan17@163.com>
2018-06-08 18:56:05 +0800

    first-commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <div class="date-head">
+      <div class="date">
+        <div class="day">12</div>
+        <div class="year-month">
+          <span class="month">MAR</span>
+          <span class="year">2018</span>
+        </div>
+      </div>
+      <div class="week">TUESDAY</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .wrapper {
+    padding: 1rem;
+    background-color: #FFF;
+  }
+</style>
